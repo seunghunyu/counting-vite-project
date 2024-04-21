5b402ab068eb818734edb716a393db339b56f463
ysh <livefor1212@gmail.com>
2024-04-21 17:57:40 +0900

    useEffect 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import "./App.css";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  useEffect(()=>{
+    console.log(`count : ${count} / input: ${input}`);
+  }, [count, input])
+//의존성 배열
+//dependency arary
+//deps
+
   const onClickButton = (value) => {
     setCount(count + value);
   };
@@ -12,6 +21,11 @@ function App() {
   return (
     <div className = "App">
       <h1>Simple Counter</h1>
+      <section>
+        <input value={input} onChange={(e)=>{
+          setInput(e.target.value);
+        }} />
+      </section>
       <section>
         <Viewer count={count} />
       </section>
